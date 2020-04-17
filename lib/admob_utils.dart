c05045914ff3aa6d648d4b5028857f047c5a8294
linusyoung <linusyoungrice@gmail.com>
2020-04-17 14:31:03 +1000

    switch to prod ad

--- lib/admob_utils.dart
+++ lib/admob_utils.dart
@@ -1,2 +1,5 @@
 const AdmobAppId = "ca-app-pub-2129001662118860~2527566896";
 const BannerAdUnitId = "ca-app-pub-2129001662118860/4962158543";
+// TEST ADID
+// const AdmobAppId = "ca-app-pub-3940256099942544~3347511713";
+// const BannerAdUnitId = "ca-app-pub-3940256099942544/6300978111";
